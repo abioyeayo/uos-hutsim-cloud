c6d007b19bd36bb6a8c75b0b696426188cfb7f10
Ayo <aoa2g15@soton.ac.uk>
2022-12-15 23:43:42 +0000

    added admin area

--- admin/index.php
+++ admin/index.php
@@ -10,36 +10,57 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Dashboard | UoS HutSim</title>
+        <title>Admin | UoS HutSim</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
         <div style="max-width: 960px; text-align: center; border: solid 1px black; margin-right: auto; margin-left: auto; padding-bottom: 20px;">
-            <h2>HutSim Simulator</h2>
-            <p>The java version running on this server is...</p> <?php echo  exec('java --version'); ?> <br>
-            <br>
-            <div style="font-weight: bold;"> 
-              <?php
-                // kill related java pid instance
-                if ($_POST['stopped_pid'] != ""){
-                      echo exec('kill '.$_POST['stopped_pid']);
-                      echo "Stopped HutSim PID: ".$_POST['stopped_pid']."<br><br>";
+          <h4 id="contributions">Active HutSim Connections</h4>
+          <div class="table-responsive">
+            <table id="member_contributions_table" class="cell-border compact stripe" style="width:100%">
+              <thead>
+                <tr>
+                  <th scope="col">Date</th>
+                  <th scope="col">Process ID</th>
+                  <th scope="col">Port Number</th>
+                  <th scope="col">View</th>
+                  <th scope="col">Disconnect</th>
+                </tr>
+              </thead>
+              <tbody>
 
-                      $sql = "UPDATE port_table SET `port_status` = 'disconnected', `process_ended` = NOW() WHERE process_id = '".$_POST['stopped_pid']."' and port_status = 'active'";
-                      if(mysqli_query($con1, $sql)){
-                          // echo "Records updated successfully.";
-                      } else{
-                          echo "Error updating record: " . $con->error;
-                          exit();
-                      }
-                }
+                <?php
 
-                
-              ?> 
-            </div>
-            <form action="hs_start.php" method="post">
-                <input type="submit" value="Start HutSim">
-            </form>
+                   // connecting to database
+                  $con = new DB_Connect();
+                  $con1=$con->connect();
+
+                  $sql = "SELECT * FROM port_table WHERE port_status ='active'";
+                  $result = mysqli_query($con1, $sql);
+                  if ( mysqli_num_rows( $result ) > 0 ) {
+                    while($row = mysqli_fetch_array($result)) {
+                      echo "<tr>
+                              <td>".$row["date_created"]."</td>
+                              <td>".$row["process_id"]."</td>
+                              <td>".$row["port_number"]."</td>
+                              <td></td>
+                              <td></td>
+                            </tr>";
+                    }
+                  }
+
+                  // close db connect
+                  $con1->close();
+
+                ?>
+
+              </tbody>
+            </table>
+          </div>
         </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
 
