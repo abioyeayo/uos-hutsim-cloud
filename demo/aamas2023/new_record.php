36da288510b446c9754e68b1914026f0397ab18e
Ayo <aoa2g15@soton.ac.uk>
2023-02-04 04:40:50 +0000

    added body padding to aamas2023 leaderboad page

--- demo/aamas2023/new_record.php
+++ demo/aamas2023/new_record.php
@@ -42,7 +42,7 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
-    <body>
+    <body class="px-4">
         <div class="shadow p-4 mb-5 bg-body-tertiary rounded" style="max-width: 960px; border: solid 1px black; margin-right: auto; margin-left: auto; margin-top:20px; padding-bottom: 20px;">
           <br>
           <h4>Add New Record</h4>
