adf99dd1ce1c869c83aba660b9ce8089b16c1e78
Ayo <aoa2g15@soton.ac.uk>
2023-02-07 15:50:52 +0000

    updated abstract and added pages and link to simulations

--- demo/aamas2023/index.php
+++ demo/aamas2023/index.php
@@ -23,7 +23,7 @@
     <body style="background-color: #efefef;">
         <div class="shadow" style="background-color: #ffffff;">
             <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom container">
-                <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                <a href="/demo/aamas2023/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                     <h4>UoS HutSim</h4>
                 </a>
 
@@ -31,8 +31,13 @@
                     <h4>AAMAS 2023 Demo</h4>
                 </div>
 
-                <div class="col-md-3 text-end">
+                <!-- <div class="col-md-3 text-end border">
+                    <a href="/demo/aamas2023/hutsim/" class="nav-link px-2 link-primary">Simulator</a>
                     <a href="/demo/aamas2023/leaderboard.php" class="nav-link px-2 link-primary">Leaderboard</a>
+                </div> -->
+                <div class="col-md-3 text-end">
+                    <button type="button" class="btn btn-sm btn-link me-2"><a href="/demo/aamas2023/hutsim/">Simulator</a></button>
+                    <button type="button" class="btn btn-sm btn-link"><a href="/demo/aamas2023/leaderboard.php">Leaderboard</a></button>
                 </div>
             </header>
         </div>
@@ -44,9 +49,9 @@
                 <div class="row mb-5 mt-4">
                     <div class="col-xxl-5 p-4">
                         <h4>Abstract</h4>
-                        <p>Autonomous swarms of robots can bring robustness, scalability and adaptivity to safety-critical tasks such as search and rescue but their application is still very limited. Using semi-autonomous swarms with human control can be a way to bring robot swarms to real-world applications. Human operators can define goals for the swarm, monitor their performance and interfere with or overrule the decisions and behaviour. In this demonstration, we show how having human operators next to the swarm can influence the performance of the system in accuracy and speed of delivering the tasks. We will use our Human Teaming Simulator, HutSim, which is unique in its simulation of aerial swarms completing tasks, from package delivery to search and rescue, with varying levels of human control. In this demonstration, we will showcase the performance gain offered by maintaining a “human-in-the-loop” over a fully autonomous system. This is illustrated in the context of search and rescue, with autonomous allocation of resources to those in need. Where a fully autonomous system will make wasted journeys through misclassification of scenes, a human-in-the-loop can overrule swarm assumptions based on broader scene context, improving both speed and accuracy.</p>
+                        <p>Autonomous swarms of robots can bring robustness, scalability and adaptability to safety-critical tasks such as search and rescue but their application is still very limited. Using semi-autonomous swarms with human control can bring robot swarms to real-world applications. Human operators can define goals for the swarm, monitor their performance and interfere with or overrule the decisions and behaviour. We designed a human swarm teaming simulator (HutSim) that allows multi-user interaction with a swarm and facilitates qualitative and quantitative user studies with a simulation of aerial swarms completing tasks, from package delivery to search and rescue, with varying levels of human control. In this demonstration, we study performance gain offered by maintaining a "human-in-the-loop" over a fully autonomous system as an example. This is illustrated in the context of search and rescue, with an autonomous allocation of resources to those in need.</p>
                     </div>
-                    <div class="col-xxl-6 text-center mt-xxl-5 pt-xxl-5">
+                    <div class="col-xxl-6 text-center mt-xxl-5 pt-xxl-3">
                         <video width="640" height="360" controls style="border: solid 2px black;">
                             <source src="uos-hutsim-aamas-2023-demo-video.mp4" type="video/mp4">
                             Your browser does not support the video tag.
