af959e8209dceb63e4b87dc35428ed564962eceb
Ayo <aoa2g15@soton.ac.uk>
2023-03-21 09:59:34 +0000

    added datatables to leaderboard

--- demo/aiuk2023/leaderboard.php
+++ demo/aiuk2023/leaderboard.php
@@ -51,6 +51,12 @@
         <title>AIUK 2023 Demo Leaderboard | UoS HutSim</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+        <style>
+        .dataTables_wrapper .dataTables_filter input {
+          margin-bottom: 20px;
+        }
+        </style>
     </head>
     <body class="px-4">
         <div style="text-align: right; max-width: 960px; margin-right: auto; margin-left: auto; padding-top: 10px; font-size: 0.9em;">
@@ -64,7 +70,8 @@
           </div>
           <h4 id="contributions">AIUK 2023 Demo Leaderboard</h4>
           <div class="table-responsive p-3">
-              <table class="table table-bordered table-sm">
+          <!-- class="table table-bordered table-sm"  -->
+              <table id="demo_table" class="compact cell-border display">
                 <thead>
                   <tr>
                     <th scope="col">Position</th>
@@ -93,7 +100,7 @@
                                 echo "<tr>";
                             }
 
-                            echo "  <td>".$s_no++."</td>
+                            echo "  <td></td>
                                     <td>".$row["fname"]."</td>
                                     <td>".$row["completion_time"]."</td>
                                     <!-- <td>".$row["task_target"]."</td> -->
@@ -112,6 +119,30 @@
         </div>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+        <script>
+          $(document).ready(function () {
+              var table = $('#demo_table').DataTable({
+                  columnDefs: [{
+                      searchable: false,
+                      orderable: false,
+                      targets: 0,
+                  }],
+                  order: [[5, 'desc']],
+                  "pageLength": 50,
+              });
+
+              // append first table column for row numbering
+              table.on('order.dt search.dt', function () {
+                  let i = 1;
+
+                  table.cells(null, 0, { search: 'applied', order: 'applied' }).every(function (cell) {
+                      this.data(i++);
+                  });
+              }).draw();
+          });
+        </script>
     </body>
 </html>
 
