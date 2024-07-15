921dbe12ec226ffe40d54fd54f4baa0ce3905676
William Hunt <w.hunt@soton.co.uk>
2024-07-15 11:58:48 +0100

    Created first version of Media page

--- sooratilab/media.php
+++ sooratilab/media.php
@@ -27,108 +27,6 @@
     <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico">
     <meta name="theme-color" content="#712cf9">
 
-
-    <style>
-        header {
-            font-size: 2em;
-            color: blue;
-        }
-
-        body {
-            background-color: #EEEEEE;
-        }
-
-
-        .topButtons {
-            margin-right: 10px; /* Adjust this value as needed */
-            color: #00ADB5; /* Change the text color */
-            background-color: #393E46; /* Change the background color */
-            border-radius: 25px; /* Add border radius to make the button round-cornered */
-            padding: 10px 20px; /* Add some padding */
-            text-decoration: none; /* Remove underline */
-            font-weight: bold; /* Make the text bold *
-        }
-
-
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
-
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
-
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
-
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
-
-      .bd-mode-toggle .dropdown-menu .active .bi {
-        display: block !important;
-      }
-    </style>
-
-
 </head>
 <body>
 <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
@@ -193,7 +91,7 @@
     </symbol>
 </svg>
 
-<div class="col-lg-8 mx-auto p-4 py-md-5">
+<div class="col-lg-10 mx-auto p-4 py-md-5">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
@@ -210,10 +108,49 @@
     </header>
 
     <main>
-        <h1 class="text-body-emphasis">Swarm Robotics Research</h1>
-        <p class="fs-5 col-md-8">Not sure what to put here. Maybe a link to the yt channel?</p>
-
-
+        <div class="row g-5">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-5">
+                <h2 class="text-body-emphasis">Demonstrating Performance Benefits of Human-Swarm Teaming</h2>
+                <p class="fs-5">A demo we took to AAMAS 2023, this work presented out Human-Swarm simulator through a usecase in which a human augmented a swarm of robots searching an area for casualties. This work was well received, and was awarded the Best Demo Award at the conference.</p>
+            </div>
+            <div class="col-md-7">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/OLUlFMo-JLI" title="YouTube video" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12 mt-4">
+        <div class="row">
+            <div class="col-md-5">
+                <h2 class="text-body-emphasis">Conversational Language Models for Human-in-the-Loop Multi-Robot Coordination</h2>
+                <p class="fs-5">A demo we took to AAMAS 2024, this work demonstrated that Large Language Models can be used as a communication and planning tool between robots through the instantiation of a dialogue.</p>
+            </div>
+            <div class="col-md-7">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/qI5HI8AS_dg" title="YouTube video" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12 mt-4">
+        <div class="row">
+            <div class="col-md-5">
+                <h2 class="text-body-emphasis">Title of you work</h2>
+                <p class="fs-5">A short description. I think an abstract is too formal so keep it easy to understand; the video has the detail.</p>
+            </div>
+            <div class="col-md-7">
+                <div class="ratio ratio-16x9">
+                    <!-- Take the URL for your video e.g. "https://www.youtube.com/watch?v=qI5HI8AS_dg", and use the code after the "v=".
+                    In this case replace "https://www.youtube.com/embed/example3" with "https://www.youtube.com/embed/qI5HI8AS_dg" -->
+                    <iframe src="https://www.youtube.com/embed/example3" title="YouTube video" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     </main>
     <footer class="pt-5 my-5 text-body-secondary border-top">
         Created by the Bootstrap team &middot; &copy; 2024
